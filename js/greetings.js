c1c032873f9fa0e1b55e0b275017909597ef00ad
syjin0630 <syjjj0630@gmail.com>
2021-09-25 18:33:01 +0900

    안내문자 보충

--- js/greetings.js
+++ js/greetings.js
@@ -16,7 +16,9 @@ function onLoginSubmit(info) {
   loginInput.classList.add(HIDDEN_CLASSNAME);
   localStorage.setItem(USERNAME_KEY, username);
   paintGreetings(username);
-  alert("이름을 클릭하면 이름을 바꿀 수 있습니다!");
+  alert(
+    "이름을 클릭하면 이름을 바꿀 수 있습니다!  (안 될 경우 새로고침을 한 번 해주세요.)"
+  );
   browser.menus.refresh();
 }
 
