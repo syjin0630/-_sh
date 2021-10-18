45d605604bb4e87dd855ee0086ee94a97403615d
syjin0630 <syjjj0630@gmail.com>
2021-10-18 23:17:28 +0900

    grid로 바꿈, 북마크 버튼만 추가

--- js/background.js
+++ js/background.js
@@ -1,4 +1,13 @@
-const images = ["1.jpg", "2.jpg", "4.jpg", "6-2.png", "10.jpg", "11.jpg"];
+const images = [
+  "1.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "6-2.png",
+  "9.jpg",
+  "10.jpg",
+  "11.jpg",
+];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
