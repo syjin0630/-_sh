50f526f1c9b13e05a7ece6bff45175c13a6b2ca5
syjin0630 <syjjj0630@gmail.com>
2022-01-03 21:23:54 +0900

    input 위치 수정, 링크 열기 수정

--- js/search.js
+++ js/search.js
@@ -24,28 +24,28 @@ function googleFound(event) {
   const googleUrl = googleInput.value;
   googleInput.value = "";
   const googlePerfectUrl = `https://www.google.com/search?q=${googleUrl}`;
-  window.open(googlePerfectUrl, "_googleUrl");
+  window.location.href = googlePerfectUrl;
 }
 function naverFound(event) {
   event.preventDefault();
   const naverUrl = naverInput.value;
   naverInput.value = "";
   const naverPerfectUrl = `https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=${naverUrl}`;
-  window.open(naverPerfectUrl, "_naverUrl");
+  window.location.href = naverPerfectUrl;
 }
 function youtubeFound(event) {
   event.preventDefault();
   const youtubeUrl = youtubeInput.value;
   youtubeInput.value = "";
   const youtubePerfectUrl = `https://www.youtube.com/results?search_query=${youtubeUrl}`;
-  window.open(youtubePerfectUrl, "_youtubeUrl");
+  window.location.href = youtubePerfectUrl;
 }
 function papagoFound(event) {
   event.preventDefault();
   const papagoText = papagoInput.value;
   papagoInput.value = "";
   const papagoUrl = `https://papago.naver.com/?sk=en&tk=ko&hn=0&st=${papagoText}`;
-  window.open(papagoUrl, "Papago");
+  window.location.href = papagoUrl;
 }
 
 search.addEventListener("submit", found);
